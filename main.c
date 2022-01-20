ea320bd1832fd26194144bf1c01753bdb94115f1
Rewen <evgeniy.movchan.03@gmail.com>
2022-01-20 21:26:36 +1000

    Final option_EN

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include "Stack.h"
+#include <windows.h>
 
 void print_kard(int sum_player, int sum_dealer, Node player1[10], Node dealer[10], int i_p, int i_d) {
-    printf("У Вас: %d очков. Карты: ", sum_player);
+    printf("Player: %d points. Cards: ", sum_player);
     for (int j = 0; j < i_p; j++) {
         printf("%s %s; ", player1[j].name, player1[j].suit);
     }
 
-    printf("\nУ Диллера: %d очков. Карты: ", sum_dealer);
+    printf("\nDiler: %d points. Cards: ", sum_dealer);
     for (int j = 0; j < i_d; j++) {
         printf("%s %s; ", dealer[j].name, dealer[j].suit);
     }
@@ -14,11 +15,11 @@ void print_kard(int sum_player, int sum_dealer, Node player1[10], Node dealer[10
 
 int main(void) {
 
-    char *name[13] = {"Двойка", "Тройка", "Четверка", "Пятерка",
-                      "Шестерка", "Семерка", "Восьмерка", "Девятка",
-                      "Десятка", "Валет", "Дама", "Король", "Туз"};
-
-    char *suit[4] = {"Пик", "Червей", "Буб", "Крести"};
+    char *name[13] = {"Two", "Three", "Four", "Five",
+                      "Six", "Seven", "Eight", "Nine",
+                      "Ten", "Jack", "Queen", "King", "Ace"};
+    // ╨┐╨╕╨║╨╕ (spades), ╤З╨╡╤А╨▓╤Л (hearts), ╨▒╤Г╨▒╨╜╤Л (diamonds), ╤В╤А╨╡╤Д╤Л (clubs)
+    char *suit[4] = {"Spades", "Hearts", "Diamonds", "Clubs"};
 
     int value[13] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
 
@@ -31,12 +32,12 @@ int main(void) {
     }
 
     Node dealer[10], player1[10];
-    char run, run1 = 'Д';
+    char run, run1 = 'Y';
 
-    printf("Начать игру? ([Д] - Да; [Н] - Нет)\n");
+    printf("Start the game? ([Y] - Yes; [N] - No)\n");
     scanf("%s", &run);
 
-    if (run == 'Д') {
+    if (run == 'Y') {
         tusov(&head);
         //printList(head);
 
@@ -57,74 +58,74 @@ int main(void) {
 
         }
 
-        printf("У Вас: %d очков. Карты: %s %s и %s %s\n", sum_player,
+        printf("Player: %d points. Cards: %s %s and %s %s\n", sum_player,
                player1[0].name, player1[0].suit,
                player1[1].name, player1[1].suit);
 
-        printf("Карты Диллера: %s %s и ***\n", dealer[0].name, dealer[0].suit);
+        printf("Diler cards: %s %s and ***\n", dealer[0].name, dealer[0].suit);
 
-        if (sum_player == 21) return printf("\nBlacJack! Вы выиграли!");
+        if (sum_player == 21) return printf("\nBlacJack! Player are winner!");
 
-        // ______________ИГРОК_______________
+        // ______________╨Ш╨У╨а╨Ю╨Ъ_______________
         int i_p;
-        if (run1 == 'Д') i_p = 2;
+        if (run1 == 'Y') i_p = 2;
         else i_p = 1;
 
-        while (run1 == 'Д') {
-            printf("\nВзять еще одну карту? ([Д] - Да; [Н] - Нет)\n");
+        while (run1 == 'Y') {
+            printf("\nTake another card? ([Y] - Yes; [N] - No)\n");
             scanf("%s", &run1);
 
-            if (run1 == 'Н') break;
+            if (run1 == 'N') break;
             player1[i_p] = (pop(&head));
 
-            if (sum_player > 21 && player1[i_p].value == 11) {
+            if ((sum_player+player1[i_p].value) > 21 && player1[i_p].value == 11) {
                 sum_player += 1;
             } else sum_player += player1[i_p].value;
 
-            printf("У Вас: %d очков. Карты: ", sum_player);
+            printf("Player: %d points. Cards: ", sum_player);
             for (int j = 0; j < (i_p + 1); j++) {
                 printf("%s %s; ", player1[j].name, player1[j].suit);
             }
             if (sum_player == 21) {
-                return printf("\nBlacJack! Вы выиграли!");
+                return printf("\nBlacJack! Player are winner!");
 
             }
             if (sum_player > 21) {
-                return printf("\nВы проиграли.");
+                return printf("\nPlayer are loser!");
             }
             i_p++;
         }
 
-        // ___________ДИЛЛЕР______________
+        // ___________╨Ф╨Ш╨Ы╨Ы╨Х╨а______________
         int i_d;
         if (sum_dealer <= 16) i_d = 2;
         else i_d = 1;
         while (sum_dealer <= 16) {
             dealer[i_d] = (pop(&head));
 
-            if (sum_d > 21 && dealer[i_d].value == 11) {
+            if ((sum_dealer + dealer[i_d].value) > 21 && dealer[i_d].value == 11) {
                 sum_dealer += 1;
             } else sum_dealer += dealer[i_d].value;
 
             i_d++;
             if (sum_dealer > 21) {
                 print_kard(sum_player, sum_dealer, player1, dealer, i_p, i_d);
-                return printf("\nВы выиграли, у диллера больше 21 очка.");
+                return printf("\nPlayer are winner, the dealer has more than 21 points.");
             }
         }
         // _______________________________
         if (sum_dealer < sum_player) {
             print_kard(sum_player, sum_dealer, player1, dealer, i_p, i_d);
-            return printf("\nВы выиграли!");
+            return printf("\nPlayer are loser!");
         }
         if (sum_dealer > sum_player) {
             print_kard(sum_player, sum_dealer, player1, dealer, i_p, i_d);
-            return printf("\nВы проиграли.");
+            return printf("\nPlayer are winner!");
         }
 
         if (sum_dealer == sum_player) {
             print_kard(sum_player, sum_dealer, player1, dealer, i_p, i_d);
-            return printf("\nНичья.");
+            return printf("\nDraw.");
         }
     }
 
