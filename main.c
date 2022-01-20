f9add82823097895f91f5d5e313947f1127320f7
Rewen <evgeniy.movchan.03@gmail.com>
2022-01-20 20:34:22 +1000

    Final option

--- main.c
+++ main.c
@@ -40,18 +40,30 @@ int main(void) {
         tusov(&head);
         //printList(head);
 
-        player1[0] = (pop(&head));
-        player1[1] = (pop(&head));
-        dealer[0] = (pop(&head));
-        dealer[1] = (pop(&head));
-        int sum_player = player1[0].value + player1[1].value;
-        int sum_dealer = dealer[0].value + dealer[1].value;
+        player1[0] = (pop(&head)); player1[1] = (pop(&head));
+        dealer[0] = (pop(&head)); dealer[1] = (pop(&head));
+        int sum_player = 0, sum_p = player1[0].value + player1[1].value;
+        int sum_dealer = 0, sum_d = dealer[0].value + dealer[1].value;
+
+        for (int i = 0; i < 2; ++i) {
+
+            if ((sum_p > 21) && (player1[i].value == 11)) {
+                sum_player += 1;
+            } else sum_player += player1[i].value;
+
+            if ((sum_d > 21) && (dealer[i].value == 11)) {
+                sum_dealer += 1;
+            } else sum_dealer += dealer[i].value;
+
+        }
 
         printf("У Вас: %d очков. Карты: %s %s и %s %s\n", sum_player,
                player1[0].name, player1[0].suit,
                player1[1].name, player1[1].suit);
 
         printf("Карты Диллера: %s %s и ***\n", dealer[0].name, dealer[0].suit);
+
+        if (sum_player == 21) return printf("\nBlacJack! Вы выиграли!");
 
         // ______________ИГРОК_______________
         int i_p;
@@ -61,44 +73,52 @@ int main(void) {
         while (run1 == 'Д') {
             printf("\nВзять еще одну карту? ([Д] - Да; [Н] - Нет)\n");
             scanf("%s", &run1);
+
             if (run1 == 'Н') break;
             player1[i_p] = (pop(&head));
-            sum_player += player1[i_p].value;
 
+            if (sum_player > 21 && player1[i_p].value == 11) {
+                sum_player += 1;
+            } else sum_player += player1[i_p].value;
 
             printf("У Вас: %d очков. Карты: ", sum_player);
-            for (int j = 0; j < i_p; j++) {
+            for (int j = 0; j < (i_p + 1); j++) {
                 printf("%s %s; ", player1[j].name, player1[j].suit);
+            }
+            if (sum_player == 21) {
+                return printf("\nBlacJack! Вы выиграли!");
+
             }
             if (sum_player > 21) {
                 return printf("\nВы проиграли.");
             }
-            if (sum_player == 21) {
-                return printf("\nBlacJack! Вы выиграли!");
-            }
             i_p++;
         }
 
-        // ___________ДИЛЛЕР_____________
+        // ___________ДИЛЛЕР______________
         int i_d;
         if (sum_dealer <= 16) i_d = 2;
         else i_d = 1;
         while (sum_dealer <= 16) {
             dealer[i_d] = (pop(&head));
-            sum_dealer += dealer[i_d].value;
+
+            if (sum_d > 21 && dealer[i_d].value == 11) {
+                sum_dealer += 1;
+            } else sum_dealer += dealer[i_d].value;
+
             i_d++;
-            if (sum_player > 21) {
+            if (sum_dealer > 21) {
                 print_kard(sum_player, sum_dealer, player1, dealer, i_p, i_d);
-                return printf("\nВы выиграли, у диллера больше 21 очков.");
+                return printf("\nВы выиграли, у диллера больше 21 очка.");
             }
         }
-
+        // _______________________________
         if (sum_dealer < sum_player) {
             print_kard(sum_player, sum_dealer, player1, dealer, i_p, i_d);
             return printf("\nВы выиграли!");
         }
         if (sum_dealer > sum_player) {
-            print_kard(sum_player, sum_dealer, player1, dealer, i_p, i_d);;
+            print_kard(sum_player, sum_dealer, player1, dealer, i_p, i_d);
             return printf("\nВы проиграли.");
         }
 
@@ -107,5 +127,6 @@ int main(void) {
             return printf("\nНичья.");
         }
     }
+
     return 0;
 }
